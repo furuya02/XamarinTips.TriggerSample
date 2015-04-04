1bf24db6d3643c0af45a58a3669a95d1ba078a0f
SIN <sin@sapporoworks.ne.jp>
2015-04-04 12:43:05 +0900

    initialize

---
+++ TriggerSample/TriggerSample.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TriggerSample.WinPhone.Resources;
+
+namespace TriggerSample.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ TriggerSample/TriggerSample/App.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TriggerSample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+    public class EntryValidation : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int n;
+            if (!Int32.TryParse(sender.Text, out n))
+            {
+                sender.TextColor = Color.Red;
+            }
+            else
+            {
+                sender.TextColor = Color.Blue;
+            }
+        }
+    }
+
+}
